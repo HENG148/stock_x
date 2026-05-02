deb75fff5752139f7d8dd997ac032310e5c0c180
R.Sokheng <support@giantfocus.com>
2026-05-02 13:37:46 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { auth } from "../auth";
+import { auth } from "./src/auth";
 
 export default auth((req) => {
   const { pathname } = req.nextUrl;
